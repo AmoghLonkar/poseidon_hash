2b541b77c4a02560ee88e333ea340d41ed00ff57
Amogh Lonkar <alonkar@kaffirlime.soe.ucsc.edu>
2021-05-21 11:10:41 -0700

    Removed state transition tet, need to add more test cases to hash tester

--- src/test/scala/poseidon/PoseidonTestSuite.scala
+++ src/test/scala/poseidon/PoseidonTestSuite.scala
@@ -21,7 +21,8 @@ import chisel3.tester.experimental.TestOptionBuilder._
             c.clock.step()
             c.io.msg.valid.poke(false.B)
             c.clock.step((p.Rf + p.Rp)*(3 + p.t*p.t))
-            //c.io.digest.valid.expect(true.B)
+            c.io.digest.valid.expect(true.B)
+            c.io.digest.bits.expect(expHash.U)
         }
         true
     }
@@ -31,24 +32,4 @@ import chisel3.tester.experimental.TestOptionBuilder._
         val m = Message("", p.t)
         testPoseidon(p, m) 
     }
-    
-    /*
-    def testStateTransitions(p: PoseidonParams, m: Message): Boolean = {
-        val hashModel = new PoseidonModel(p)
-        val expHash = hashModel(m)
-        test(new Poseidon(p)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
-            c.io.msg.bits.poke(m.string2BigInt.U)
-            c.io.msg.valid.poke(true.B)
-            c.state.peek()
-        }
-        true
-    }
-
-    "Hardware Poseidon module should undergo correct state transitions" in {
-        val p = PoseidonParams(r = 64, c = 64, Rf = 8, Rp = 57, alpha = 5)
-        val m = Message("", p.t)
-
-        testStateTransitions(p, m)
-    }
-    */
  }
