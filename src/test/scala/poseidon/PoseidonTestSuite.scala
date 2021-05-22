0cd147f27829ea3cf0c5e9d58d7b4335b04ee6a8
Amogh Lonkar <alonkar@kaffirlime.soe.ucsc.edu>
2021-05-21 17:47:46 -0700

    Updated messages in existing tests and added parallel matmul tests

--- src/test/scala/poseidon/PoseidonTestSuite.scala
+++ src/test/scala/poseidon/PoseidonTestSuite.scala
@@ -20,7 +20,7 @@ import chisel3.tester.experimental.TestOptionBuilder._
             c.io.msg.bits.poke(m.string2BigInt.U)
             c.clock.step()
             c.io.msg.valid.poke(false.B)
-            c.clock.step((p.Rf + p.Rp)*(3 + p.t*p.t))
+            c.clock.step((p.Rf + p.Rp)*(3 + p.t*p.t/p.parallelism))
             c.io.digest.valid.expect(true.B)
             c.io.digest.bits.expect(expHash.U)
         }
@@ -35,13 +35,31 @@ import chisel3.tester.experimental.TestOptionBuilder._
 
     "Hardware Poseidon module should compute correct hash of 'abc'" in {
         val p = PoseidonParams(r = 64, c = 64, Rf = 8, Rp = 57, alpha = 5)
-        val m = Message("", p.t)
+        val m = Message("abc", p.t)
         testPoseidon(p, m) 
     }
 
     "Hardware Poseidon module should compute correct hash of 'Chisel is too much fun!'" in {
         val p = PoseidonParams(r = 64, c = 64, Rf = 8, Rp = 57, alpha = 5)
+        val m = Message("Chisel is too much fun!", p.t)
+        testPoseidon(p, m) 
+    }
+
+    "Hardware Poseidon module should compute correct hash of '' using parallel MatMul" in {
+        val p = PoseidonParams(r = 64, c = 64, Rf = 8, Rp = 57, alpha = 5, parallelism = 3)
         val m = Message("", p.t)
+        testPoseidon(p, m) 
+    }
+
+    "Hardware Poseidon module should compute correct hash of 'abc' using parallel MatMul" in {
+        val p = PoseidonParams(r = 64, c = 64, Rf = 8, Rp = 57, alpha = 5, parallelism = 3)
+        val m = Message("abc", p.t)
+        testPoseidon(p, m) 
+    }
+
+    "Hardware Poseidon module should compute correct hash of 'Chisel is too much fun!' using parallel MatMul" in {
+        val p = PoseidonParams(r = 64, c = 64, Rf = 8, Rp = 57, alpha = 5, parallelism = 3)
+        val m = Message("Chisel is too much fun!", p.t)
         testPoseidon(p, m) 
     }
  }
