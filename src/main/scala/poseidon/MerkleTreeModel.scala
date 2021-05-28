1ba270054a575817f5235a38a1da03e983dee55f
Amogh Lonkar <alonkar@kaffirlime.soe.ucsc.edu>
2021-05-28 15:59:59 -0700

    Added function to update hash value

--- src/main/scala/poseidon/MerkleTreeModel.scala
+++ src/main/scala/poseidon/MerkleTreeModel.scala
@@ -9,9 +9,13 @@ case class MerkleParams(p: PoseidonParams, numChild: Int = 2){
 }
 
 
-case class Node(m: MerkleParams, var data: Message, children: Seq[Int]) {
+case class Node(m: MerkleParams, val data: Message, children: Seq[Int]) {
     val hashFn = new PoseidonModel(m.p)
-    val hash = hashFn(data)
+    var hash = hashFn(data)
+
+    def computeHash(in_val: Message) = {
+        hash = hashFn(in_val)
+    } 
 }
 
 object MerkleTreeModel{
@@ -23,20 +27,19 @@ object MerkleTreeModel{
         val tree = new ArrayBuffer[Node]()
         val numNodes: Int = (0 to log2ceil(inputs.size)).map( i => (inputs.size/pow(m.numChild, i)).ceil).sum.toInt
         (0 until numNodes).foreach(i => if(i < numNodes - inputs.size) { tree += Node(m, Message("", m.p.t), Seq.tabulate(m.numChild)(j => m.numChild*i + j+1)) } else { tree += Node(m, inputs(i+1 - inputs.size), Seq.fill(1)(i)) })
-
         //Iterate up the tree to get final hash
         for(i <- numNodes - inputs.size - 1 to 0 by -1){
             val hashList = new ArrayBuffer[String]()
             tree(i).children.map { j =>hashList += tree(j).hash.toString}
             val inString: String = hashList.mkString("")
-            tree(i).data = Message(inString, m.p.t)
+            tree(i).computeHash(Message(inString, m.p.t))
         }
 
         tree 
     }
 }
 
-class MerkleTreeModel(m: MerkleParams, val nodes: ArrayBuffer[Node]){
+class MerkleTreeModel(m: MerkleParams){
 
     def apply(origContents: Seq[Message]): BigInt = {
         val tree = MerkleTreeModel.BuildTree(m, origContents)
