6834d7fd8ff33553de8bb18ee01d777b65acac13
Amogh Lonkar <alonkar@kaffirlime.soe.ucsc.edu>
2021-05-28 09:19:08 -0700

    ABuilt leaf nodes, compute intermediate and root

--- src/main/scala/poseidon/MerkleTreeModel.scala
+++ src/main/scala/poseidon/MerkleTreeModel.scala
@@ -2,31 +2,40 @@ package poseidon
 
 import chisel3._ 
 import scala.collection.mutable.ArrayBuffer
-import scala.collection.script.Message
-
-//Inspiration for implementation taken
-//from https://blog.genuine.com/2020/02/merkle-tree-implementation-in-scala/
 
 case class MerkleParams(p: PoseidonParams, numChild: Int = 2){
 
 }
 
 
-class Node(data: BigInt, children: ArrayBuffer[Option[Node]]) {
-    this.data = data
-    this.children = children
+class Node(m: MerkleParams, data: BigInt, child: Int) {
+    var data = data
 }
 
 object MerkleTreeModel{
-    def BuildTree(leafNodes: ArrayBuffer[Message]): ArrayBuffer[Node] = {
+   
+    def log2ceil(in: Int): Int = { (log10(int)/log10(2)).ceil.toInt }
 
+    def BuildTree(m: MerkleParams, inputs: Seq[Message]): ArrayBuffer[Node] = {
+        //Initialize tree
+        val tree = new ArrayBuffer[Node]()
+        val numNodes: Int = (0 to log2ceil(leafData.size)).map( i => (leafData.size/pow(m.numChild, i)).ceil).sum.toInt
+        (0 until numNodes).foreach(i => tree += Node(m, BigInt(0), ))
+
+        //Get Hash values of each input and convert to leaf nodes
+        val hashList = Seq.fill(inputs.size)(new PoseidonModel(m.p))
+        val leafData = hashList.zip(inputs).map { case(a, b) => a(b) }
+
+        (0 until inputs.size).foreach(i => tree(i).data = leafData(i))
+
+        //Iterate up the tree to get 
     }
 }
 
 class MerkleTreeModel(m: MerkleParams, val nodes: ArrayBuffer[Node]){
 
-    def apply(origContents: ArrayBuffer[Message]): BigInt = {
-        val tree = BuildTree(origContents)
+    def apply(origContents: Seq[Message]): BigInt = {
+        val tree = BuildTree(m, origContents)
         tree(0).data
     }
 }
