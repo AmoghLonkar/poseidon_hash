343839bfface023c39ef5217f3115707ecee9b9c
Amogh Lonkar <alonkar@kaffirlime.soe.ucsc.edu>
2021-05-28 17:13:37 -0700

    Added test for 4-to-1 tree

--- src/test/scala/poseidon/MerkleTreeModelTestSuite.scala
+++ src/test/scala/poseidon/MerkleTreeModelTestSuite.scala
@@ -8,13 +8,12 @@ import treadle._
 import chisel3.tester.experimental.TestOptionBuilder._ 
 
 import scala.collection.mutable.ArrayBuffer
-import scala.collection.script.Message
 
 class MerkleTreeModelTester extends FreeSpec with ChiselScalatestTester {
     "Simple 2-to-1 Merkle Tree should return the proper hash" in {
         val p = PoseidonParams(r = 64, c = 64, Rf = 8, Rp = 57, alpha = 5)
-        val msg1 = Message("", 3)
-        val msg2 = Message("", 3)
+        val msg1 = Message("a", 3)
+        val msg2 = Message("b", 3)
 
         val inSeq = Seq(msg1, msg2)
 
@@ -30,6 +29,35 @@ class MerkleTreeModelTester extends FreeSpec with ChiselScalatestTester {
         val exp_out: BigInt = final_perm(Message(finalIn, 3))
 
         val m = MerkleParams(p)
+        val mTree = new MerkleTreeModel(m)
+        val model_out: BigInt = mTree(inSeq)
+        assert(model_out == exp_out)
+    }
+
+    "Simple 4-to-1 Merkle Tree should return the proper hash" in {
+        val p = PoseidonParams(r = 64, c = 64, Rf = 8, Rp = 57, alpha = 5)
+        val msg1 = Message("a", 3)
+        val msg2 = Message("b", 3)
+        val msg3 = Message("c", 3)
+        val msg4 = Message("d", 3)
+
+        val inSeq = Seq(msg1, msg2, msg3, msg4)
+        val perm1 = new PoseidonModel(p)
+        val perm2 = new PoseidonModel(p)
+        val perm3 = new PoseidonModel(p)
+        val perm4 = new PoseidonModel(p)
+
+        val hash1: BigInt = perm1(msg1) 
+        val hash2: BigInt = perm2(msg2)
+        val hash3: BigInt = perm3(msg3)
+        val hash4: BigInt = perm4(msg4)
+
+        val finalIn: String = hash1.toString + hash2.toString + hash3.toString + hash4.toString
+
+        val final_perm = new PoseidonModel(p)
+        val exp_out: BigInt = final_perm(Message(finalIn, 3))
+
+        val m = MerkleParams(p, 4)
         val mTree = new MerkleTreeModel(m)
         val model_out: BigInt = mTree(inSeq)
         assert(model_out == exp_out)
