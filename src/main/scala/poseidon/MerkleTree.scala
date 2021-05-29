24edac6666cf74e1403a9fe2fbec7c63456234ae
Amogh Lonkar <alonkar@kaffirlime.soe.ucsc.edu>
2021-05-29 15:22:58 -0700

    Figure out how to instantiate module within class

--- src/main/scala/poseidon/MerkleTree.scala
+++ src/main/scala/poseidon/MerkleTree.scala
@@ -11,6 +11,19 @@ object MerkleTree {
     val idle :: loading :: hashing :: Nil = Enum(3)
 }
 
+class HWNode (m: MerkleParams) extends Module {
+    val io = IO(new Bundle {
+        val in_data: UInt = Input(UInt((m.p.hashLen * 8).W))
+        val children = Input(Vec(m.numChild, UInt((log2Ceil(m.numNodes)).W)))
+        val hash: UInt = Output(UInt((m.p.hashLen * 8).W))
+    })
+
+    val permutation = Module(new Poseidon(m.p))
+    permutation.io.msg.valid := true.B
+    permutation.io.msg.bits := io.in_data
+    io.hash := permutation.io.digest.bits
+}
+
 class MerkleTreeIO(m: MerkleParams) extends Bundle {
     val msg = Flipped(Decoupled(UInt((m.p.msgLen * 8).W)))
     val digest = Decoupled(UInt((m.p.hashLen * 8).W)) 
@@ -23,14 +36,16 @@ class MerkleTree(m: MerkleParams) extends Module {
     val state = RegInit(MerkleTree.idle)
     io.msg.ready := (state === (MerkleTree.idle))
     //To hold inputs to leaf nodes
-    val inSeq = Reg(Vec(m.inputSize, UInt((m.p.msgLen*8).W)))
+    val inSeq = Reg(Vec(m.numInputs, UInt((m.p.msgLen*8).W)))
 
+    //Register to hold tree
+    val tree = Reg(Vec(m.numNodes, Module(new HWNode(m))))
     //Counter initialization
-    val (loadingCount, loadingDone) = Counter(0 until m.inputSize, state === MerkleTree.loading)
+    val (loadingCount, loadingDone) = Counter(0 until m.numInputs, state === MerkleTree.loading)
 
     switch(state){
         is(MerkleTree.idle) {
-            inSeq := VecInit(Seq.fill(m.inputSize)(0.U))
+            inSeq := VecInit(Seq.fill(m.numInputs)(0.U))
             
             when(io.msg.valid && io.msg.ready){
               state := MerkleTree.loading
