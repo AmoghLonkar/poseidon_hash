62410d671988522288b015323546d27e3d00bd1b
Amogh Lonkar <alonkar@kaffirlime.soe.ucsc.edu>
2021-05-29 14:50:09 -0700

    Included input poking in test function

--- src/test/scala/poseidon/MerkleTreeTestSuite.scala
+++ src/test/scala/poseidon/MerkleTreeTestSuite.scala
@@ -6,22 +6,28 @@ import org.scalatest.FreeSpec
 
 import treadle._
 import chisel3.tester.experimental.TestOptionBuilder._
-import scala.collection.script.Message
 
 class MerkleTreeTester extends FreeSpec with ChiselScalatestTester {
 
     def testMerkleTree(m: MerkleParams, inSeq: Seq[Message]): Boolean = {
         val treeModel = new MerkleTreeModel(m)
         val exp_out = treeModel(inSeq)
-        test(new Poseidon(p)).withAnnotations(Seq(WriteVcdAnnotation)) { c => 
-            
+        test(new MerkleTree(m)).withAnnotations(Seq(WriteVcdAnnotation)) { c => 
+            c.io.msg.valid.poke(true.B)
+            c.io.msg.ready.expect(true.B)
+            c.io.digest.valid.expect(false.B)
+            c.clock.step()
+            c.io.msg.bits.poke(inSeq(0).string2BigInt.U)
+            c.clock.step()
+            c.io.msg.bits.poke(inSeq(1).string2BigInt.U)
+            c.clock.step()
         }
         true
     }
 
     "Hardware MerkleTree root should store the correct value in simple 2-to-1 tree" in {
-        val p = PoseidonParams(r = 64, c = 64, Rf = 8, Rp = 57, alpha = 5)
-        val m = MerkleParams(p, 2)
+        val p = PoseidonParams(r = 64, c = 64, Rf = 8, Rp = 57, alpha = 5, parallelism = 3)
+        val m = MerkleParams(p, 2, 2)
 
         val msg1 = Message("a", 3)
         val msg2 = Message("b", 3)
