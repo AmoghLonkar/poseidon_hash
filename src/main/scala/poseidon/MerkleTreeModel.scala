b2e91d8547b2e6675ad0775b01ccb229fe2f39a2
Amogh Lonkar <alonkar@kaffirlime.soe.ucsc.edu>
2021-05-29 14:56:09 -0700

    AdAdded numNode calculation in parameters

--- src/main/scala/poseidon/MerkleTreeModel.scala
+++ src/main/scala/poseidon/MerkleTreeModel.scala
@@ -4,8 +4,9 @@ import chisel3._
 import scala.math._
 import scala.collection.mutable.ArrayBuffer
 
-case class MerkleParams(p: PoseidonParams, numChild: Int = 2){
-
+case class MerkleParams(p: PoseidonParams, numInputs: Int, numChild: Int = 2){
+    def lognceil(in: Int, size: Int): Int = { (log10(in)/log10(size)).ceil.toInt }
+    val numNodes: Int = (0 to lognceil(numInputs, numChild)).map( i => (numInputs/pow(numChild, i)).ceil).sum.toInt
 }
 
 
@@ -20,16 +21,13 @@ case class Node(m: MerkleParams, val data: Message, children: Seq[Int]) {
 
 object MerkleTreeModel{
    
-    def lognceil(in: Int, size: Int): Int = { (log10(in)/log10(size)).ceil.toInt }
-
     def BuildTree(m: MerkleParams, inputs: Seq[Message]): ArrayBuffer[Node] = {
         //Initialize tree
         val tree = new ArrayBuffer[Node]()
-        val numNodes: Int = (0 to lognceil(inputs.size, m.numChild)).map( i => (inputs.size/pow(m.numChild, i)).ceil).sum.toInt
 
-        (0 until numNodes).foreach(i => if(i < numNodes - inputs.size) { tree += Node(m, Message("", m.p.t), Seq.tabulate(m.numChild)(j => m.numChild*i + j+1)) } else { tree += Node(m, inputs(i - (inputs.size - m.numChild) - 1), Seq.fill(1)(i)) })
+        (0 until m.numNodes).foreach(i => if(i < m.numNodes - inputs.size) { tree += Node(m, Message("", m.p.t), Seq.tabulate(m.numChild)(j => m.numChild*i + j+1)) } else { tree += Node(m, inputs(i - (inputs.size - m.numChild) - 1), Seq.fill(1)(i)) })
         //Iterate up the tree to get final hash
-        for(i <- numNodes - inputs.size - 1 to 0 by -1){
+        for(i <- m.numNodes - inputs.size - 1 to 0 by -1){
             val hashList = new ArrayBuffer[String]()
             tree(i).children.map { j =>hashList += tree(j).hash.toString}
             val inString: String = hashList.mkString("")
