6599f607b5e99c5f5c6d410ed5558d44b0b0f86d
Amogh Lonkar <alonkar@kaffirlime.soe.ucsc.edu>
2021-06-05 17:12:36 -0700

    Added tree height for clock stepping in test function

--- src/test/scala/poseidon/MerkleTreeTestSuite.scala
+++ src/test/scala/poseidon/MerkleTreeTestSuite.scala
@@ -7,12 +7,15 @@ import org.scalatest.FreeSpec
 import treadle._
 import chisel3.tester.experimental.TestOptionBuilder._
 
+import scala.math._
+
 class MerkleTreeTester extends FreeSpec with ChiselScalatestTester {
 
     def testMerkleTree(m: MerkleParams, inSeq: Seq[Message]): Boolean = {
         val treeModel = new MerkleTreeModel(m)
+        val treeHeight = (log10(m.numNodes)/log10(m.numChild)).ceil.toInt
         val exp_out = treeModel(inSeq)
-        test(new MerkleTree(m)).withAnnotations(Seq(WriteVcdAnnotation)) { c => 
+        test(new MerkleTree(m)) { c => 
             c.io.msg.valid.poke(true.B)
             c.io.msg.ready.expect(true.B)
             c.io.digest.valid.expect(false.B)
@@ -20,7 +23,7 @@ class MerkleTreeTester extends FreeSpec with ChiselScalatestTester {
             c.io.msg.bits.poke(inSeq(0).string2BigInt.U)
             c.clock.step()
             c.io.msg.bits.poke(inSeq(1).string2BigInt.U)
-            c.clock.step((m.numNodes - 1)*((m.p.Rf + m.p.Rp)*(3 + m.p.t*m.p.t/m.p.parallelism)))
+            c.clock.step((treeHeight)*((m.p.Rf + m.p.Rp)*(3 + m.p.t*m.p.t/m.p.parallelism)))
             c.clock.step()
             c.io.digest.valid.expect(true.B)
             c.clock.step(4)
@@ -36,6 +39,19 @@ class MerkleTreeTester extends FreeSpec with ChiselScalatestTester {
         val msg1 = Message("a", 3)
         val msg2 = Message("b", 3)
         val inSeq = Seq(msg1, msg2)
+
+        testMerkleTree(m, inSeq)
+    }
+
+    "Hardware MerkleTree root should store the correct value in simple 4-to-1 tree" in {
+        val p = PoseidonParams(r = 64, c = 64, Rf = 8, Rp = 57, alpha = 5, parallelism = 3)
+        val m = MerkleParams(p, 4, 4)
+
+        val msg1 = Message("a", 3)
+        val msg2 = Message("b", 3)
+        val msg3 = Message("c", 3)
+        val msg4 = Message("d", 3)
+        val inSeq = Seq(msg1, msg2, msg3, msg4)
 
         testMerkleTree(m, inSeq)
     }
